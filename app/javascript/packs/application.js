2c0e4dcdf41fe862e45615d1bc5c5a16c563ef59
Will Thompson <will.f.thompson@gmail.com>
2020-10-06 13:03:21 +0200

    dark mode and footer removal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,13 +24,14 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { initBW } from '../plugins/init_bw';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
   document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-
+  initBW();
 
 });
 
---
+++ app/javascript/plugins/init_bw.js
@@ -0,0 +1,39 @@
+const initBW = () =>{
+  const toggle = document.querySelector('#bw');
+  const home = document.querySelectorAll('.home-container');
+  const welcome = document.querySelector('.welcomebg')
+  const card = document.querySelectorAll('.project-card')
+  const cardlink = document.querySelectorAll('.project-card a')
+  const show = document.querySelector('.show-content');
+
+
+  toggle.addEventListener('load', makeBW)
+  toggle.addEventListener('click', makeBW)
+
+  function makeBW() {
+    if(show){
+      show.classList.toggle('black');
+    }
+    if(home){
+      home.forEach(element => {
+        element.classList.toggle('black');
+        element.classList.toggle('dark-border');
+      });
+    if(card){
+      card.forEach(e => {
+        e.classList.toggle('white-border');
+      })
+      cardlink.forEach(e => {
+        e.classList.toggle('black');
+      })
+    }
+
+    }
+    if(welcome){
+      welcome.classList.toggle('black');
+    }
+  }
+}
+
+
+export { initBW };
